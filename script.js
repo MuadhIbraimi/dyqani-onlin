edbf97f332f89b6dce2e407668f9f24c0ed0949e
MuadhIbraimi <muadhibraimi39@gmail.com>
2026-02-06 01:22:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function selectProduct(productName) {
+document.getElementById('produktInput').value = productName;
+document.getElementById('orderForm').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Dërgimi i formularit në Google Sheets
+document.getElementById('orderForm').addEventListener('submit', function(e) {
+e.preventDefault();
+
+fetch("TU_WEB_APP_URL", { // vendos këtu Web App URL nga Apps Script
+method: "POST",
+body: new URLSearchParams(new FormData(this))
+})
+.then(response => response.text())
+.then(data => {
+document.getElementById('successMsg').style.display = 'block';
+document.getElementById('orderForm').reset();
+})
+.catch(error => alert("Gabim në regjistrim: " + error));
+});
